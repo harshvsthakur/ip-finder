167ebd63f8135dd35dbe3781af7688be35d491d6
Harshvardhan Singh Thakur <63444563+harshvsthakur@users.noreply.github.com>
2021-06-02 00:22:52 +0400

    Add files via upload

---
+++ ip finder.py
@@ -0,0 +1,14 @@
+import socket
+import csv
+
+for i in range(0,11):
+    IP = '192.168.0.'+str(i)
+    try:
+        x = socket.gethostbyaddr(IP)
+        t = list(x[0],x[2][0])
+        with open(r'C:\Users\harshvardhans\Desktop\test.csv','rb') as out:
+            csv_out=csv.writer(out)
+            csv_out.writerow(['name','ip'])
+            csv_out.writerow(t)
+    except:
+        print('{} NA'.format(IP))
